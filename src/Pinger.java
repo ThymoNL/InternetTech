7026996f420adeb53777564c69d6cb439cc9c450
SanderAdam00 <39301402+SanderAdam00@users.noreply.github.com>
2018-11-27 13:46:44 +0100

    It can send "Ping" now!

--- src/Pinger.java
+++ src/Pinger.java
@@ -1,7 +1,12 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Pinger implements Runnable {
-	Socket socket;
+	private Socket socket;
+	private String expectedResponse = "Pong";
 
 	public Pinger(Socket socket) {
 		this.socket = socket;
@@ -11,6 +16,21 @@ public class Pinger implements Runnable {
 	public void run() {
 		while (socket.isConnected()) {
 
+			try {
+				PrintWriter out = new PrintWriter(socket.getOutputStream());
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+				out.println("Ping");
+				out.flush();
+
+				Thread thread =  new Thread();
+				if(in.equals(expectedResponse)){
+
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 		}
 	}
 }
