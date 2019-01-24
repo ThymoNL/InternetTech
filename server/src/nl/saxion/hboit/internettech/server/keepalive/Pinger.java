f6d64973510fb406df1dd05364a64173d7de0ed0
Thymo van Beers <thymovanbeers@gmail.com>
2019-01-24 13:35:34 +0100

    Some fixes to Pinger

--- server/src/nl/saxion/hboit/internettech/server/keepalive/Pinger.java
+++ server/src/nl/saxion/hboit/internettech/server/keepalive/Pinger.java
@@ -7,7 +7,8 @@ import java.io.PrintWriter;
 import java.net.Socket;
 
 public class Pinger implements Runnable {
-	private static final int TIMEOUT = 3000;
+	private static final int INTERVAL = 60000; // Ping interval
+	private static final int TIMEOUT = 3000; // Max. pong reply time
 
 	private Socket socket;
 	private InputStream is;
@@ -37,13 +38,13 @@ public class Pinger implements Runnable {
 
 			while (ping && socket.isConnected()) {
 				pongTime = -1; // Reset timer to something we know is invalid
-				Thread.sleep(60 * 1000);
+				Thread.sleep(INTERVAL);
 
 				out.println("PING");
 				out.flush();
 				long pingTime = System.currentTimeMillis();
 
-				Thread.sleep(3 * 1000);
+				Thread.sleep(TIMEOUT);
 				if (pongTime == -1 || pongTime - pingTime > TIMEOUT) {
 					cb.onTimeout();
 					ping = false;
