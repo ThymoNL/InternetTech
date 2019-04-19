bec71c718006e8cf091f448bc7557b69906953ba
Thymo van Beers <thymovanbeers@gmail.com>
2019-04-19 16:21:06 +0200

    Add color to server

--- server/src/nl/saxion/hboit/internettech/server/color/ANSIColor.java
+++ server/src/nl/saxion/hboit/internettech/server/color/ANSIColor.java
@@ -1,6 +1,10 @@
 package nl.saxion.hboit.internettech.server.color;
 
 public class ANSIColor {
+	public static final String RESET = "\u001B[0m";
+	public static final String RED = "\u001B[31m";
+	public static final String GREEN = "\u001B[32m";
 
-	private ANSIColor() {}
+	private ANSIColor() {
+	}
 }
